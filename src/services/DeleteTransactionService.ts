5545163b9e2c5eb122a5d8218c184a2b8e43837e
Sergio Pereira <sergiohgp@gmail.com>
2020-10-06 00:55:48 -0400

    Update to link front-end

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '../errors/AppError';
 
 import TransactionRepository from '../repositories/TransactionsRepository';
-import Transaction from '../models/Transaction';
+// import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
   public async execute(id: string): Promise<void> {
